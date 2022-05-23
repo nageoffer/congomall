ba330588b73be82bfbf0d58b78fcb85dde317cbe
chen.ma <machencoding@163.com>
2022-05-23 20:39:58 +0800

    添加支付模块

---
+++ mall4j-pay/src/main/java/cn/mall4j/biz/pay/Mall4jPayApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jPayApplication.class, args);
+    }
+}
