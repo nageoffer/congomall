92ad7d00d4f955308022b057b341773693993905
chen.ma <machencoding@163.com>
2022-05-23 20:12:21 +0800

    添加基础数据模块

---
+++ mall4j-base-data/src/main/java/cn/mall4j/mall4jbasedata/Mall4jBaseDataApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.mall4jbasedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jBaseDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jBaseDataApplication.class, args);
+    }
+}
