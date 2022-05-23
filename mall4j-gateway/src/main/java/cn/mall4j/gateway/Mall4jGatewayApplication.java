092d63d01561a03e6e9b7c7e669dd4dd8c423d60
chen.ma <machencoding@163.com>
2022-05-23 19:12:33 +0800

    添加 mall4j-gateway 模块

---
+++ mall4j-gateway/src/main/java/cn/mall4j/gateway/Mall4jGatewayApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jGatewayApplication.class, args);
+    }
+}
