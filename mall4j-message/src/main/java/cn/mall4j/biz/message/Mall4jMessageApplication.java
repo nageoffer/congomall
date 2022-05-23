55fbc3de93b5ab31aeb11badaaba02d92505d695
chen.ma <machencoding@163.com>
2022-05-23 20:35:59 +0800

    添加消息推送模块

---
+++ mall4j-message/src/main/java/cn/mall4j/biz/message/Mall4jMessageApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jMessageApplication.class, args);
+    }
+}
