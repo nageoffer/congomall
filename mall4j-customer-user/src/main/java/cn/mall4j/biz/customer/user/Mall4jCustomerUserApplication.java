7361daea16fd8cb4d6a608ee1b7ac19752e5da0e
chen.ma <machencoding@163.com>
2022-05-23 20:10:09 +0800

    添加 c 端用户模块

---
+++ mall4j-customer-user/src/main/java/cn/mall4j/biz/customer/user/Mall4jCustomerUserApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.customer.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jCustomerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jCustomerUserApplication.class, args);
+    }
+}
