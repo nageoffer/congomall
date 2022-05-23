54e6baae2a5770678305091cc7bfec0fda8933e2
chen.ma <machencoding@163.com>
2022-05-23 20:29:33 +0800

    添加优惠券模块

---
+++ mall4j-coupon/src/main/java/cn/mall4j/biz/coupon/Mall4jCouponApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jCouponApplication.class, args);
+    }
+}
