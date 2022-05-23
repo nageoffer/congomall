afb46f20fa2438944201cecf044364d767fd2574
chen.ma <machencoding@163.com>
2022-05-23 20:33:03 +0800

    添加购物车模块

---
+++ mall4j-cart/src/main/java/cn/mall4j/biz/cart/Mall4jCartApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jCartApplication.class, args);
+    }
+}
