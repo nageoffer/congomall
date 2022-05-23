1ef88d61beb2e6a1b6e0f10c1a1778bf7692a9f6
chen.ma <machencoding@163.com>
2022-05-23 20:34:31 +0800

    添加商品模块

---
+++ mall4j-product/src/main/java/cn/mall4j/biz/product/Mall4jProductApplication.java
@@ -0,0 +1,12 @@
+package cn.mall4j.biz.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4jProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4jProductApplication.class, args);
+    }
+}
