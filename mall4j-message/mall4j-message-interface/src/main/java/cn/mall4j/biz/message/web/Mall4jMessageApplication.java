e93c203eb719fb4276efeb66e1fbbd300d4c50d0
chen.ma <machencoding@163.com>
2022-05-29 14:39:14 +0800

    添加消息模块服务注册

--- mall4j-message/mall4j-message-interface/src/main/java/cn/mall4j/biz/message/web/Mall4jMessageApplication.java
+++ mall4j-message/mall4j-message-interface/src/main/java/cn/mall4j/biz/message/web/Mall4jMessageApplication.java
@@ -19,7 +19,9 @@ package cn.mall4j.biz.message.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "cn.mall4j.biz.message")
 public class Mall4jMessageApplication {
     
