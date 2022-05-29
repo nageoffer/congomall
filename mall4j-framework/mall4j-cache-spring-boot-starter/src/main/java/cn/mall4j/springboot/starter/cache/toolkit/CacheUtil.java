cd4f2322acb0005aec8f01c2ec1d9217d1ec52f9
chen.ma <machencoding@163.com>
2022-05-29 22:44:52 +0800

    添加缓存工具类

---
+++ mall4j-framework/mall4j-cache-spring-boot-starter/src/main/java/cn/mall4j/springboot/starter/cache/toolkit/CacheUtil.java
@@ -0,0 +1,26 @@
+package cn.mall4j.springboot.starter.cache.toolkit;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * 缓存工具类
+ */
+public class CacheUtil {
+    
+    private static final String SPLICING_OPERATOR = "_";
+    
+    /**
+     * 构建缓存标识
+     *
+     * @param keys
+     * @return
+     */
+    public static String buildKey(String... keys) {
+        Stream.of(keys).forEach(each -> Optional.ofNullable(Strings.emptyToNull(each)).orElseThrow(() -> new RuntimeException("构建缓存 key 不允许为空")));
+        return Joiner.on(SPLICING_OPERATOR).join(keys);
+    }
+}
