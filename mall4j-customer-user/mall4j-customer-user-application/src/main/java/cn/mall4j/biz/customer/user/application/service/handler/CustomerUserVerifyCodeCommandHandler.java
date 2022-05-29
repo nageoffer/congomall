5f371a029336966ce32f0cdb8d21b6c419eee9ca
chen.ma <machencoding@163.com>
2022-05-29 18:02:01 +0800

    C 端用户注册时通过 redis 缓存验证码

--- mall4j-customer-user/mall4j-customer-user-application/src/main/java/cn/mall4j/biz/customer/user/application/service/handler/CustomerUserVerifyCodeCommandHandler.java
+++ mall4j-customer-user/mall4j-customer-user-application/src/main/java/cn/mall4j/biz/customer/user/application/service/handler/CustomerUserVerifyCodeCommandHandler.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.mall4j.biz.customer.user.application.service.handler;
 
 import cn.hutool.core.util.RandomUtil;
@@ -5,10 +22,13 @@ import cn.mall4j.biz.customer.user.application.req.UserVerifyCodeCommand;
 import cn.mall4j.biz.customer.user.infrastructure.remote.MessageSendRemoteService;
 import cn.mall4j.biz.customer.user.infrastructure.remote.dto.MailSendRemoteCommand;
 import cn.mall4j.ddd.framework.core.domain.CommandHandler;
+import cn.mall4j.springboot.starter.cache.DistributedCache;
 import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import static cn.mall4j.biz.customer.user.common.CacheConstant.REGISTER_USER_VERIFY_CODE;
 
 /**
  * C 端用户验证码
@@ -19,6 +39,8 @@ public class CustomerUserVerifyCodeCommandHandler implements CommandHandler<User
     
     private final MessageSendRemoteService messageSendRemoteService;
     
+    private final DistributedCache distributedCache;
+    
     @Value("${customer.user.register.verify.sender}")
     private String sender;
     
@@ -28,6 +50,7 @@ public class CustomerUserVerifyCodeCommandHandler implements CommandHandler<User
     @Override
     public Boolean handler(UserVerifyCodeCommand requestParam) {
         String verifyCode = RandomUtil.randomNumbers(6);
+        distributedCache.put(REGISTER_USER_VERIFY_CODE, verifyCode);
         MailSendRemoteCommand remoteCommand = new MailSendRemoteCommand();
         remoteCommand.setTitle("Mall4J邮箱验证码提醒")
                 .setReceiver(requestParam.getReceiver())
