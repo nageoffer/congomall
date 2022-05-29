3c8df91096cdbf54acb39fe9c9b112174e799817
chen.ma <machencoding@163.com>
2022-05-29 16:44:06 +0800

    抽象缓存基础组件

---
+++ mall4j-framework/mall4j-cache-spring-boot-starter/src/main/java/cn/mall4j/springboot/starter/cache/RedisTemplateProxy.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.mall4j.springboot.starter.cache;
+
+import cn.mall4j.springboot.starter.cache.config.RedisDistributedProperties;
+import cn.mall4j.springboot.starter.cache.toolkit.FastJson2Util;
+import com.alibaba.fastjson2.JSON;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * Redis 缓存代理
+ */
+@RequiredArgsConstructor
+public class RedisTemplateProxy implements DistributedCache {
+    
+    private final StringRedisTemplate stringRedisTemplate;
+    
+    private final RedisDistributedProperties redisProperties;
+    
+    @Override
+    public <T> T get(String key, Class<T> clazz) {
+        String value = stringRedisTemplate.opsForValue().get(key);
+        if (String.class.isAssignableFrom(clazz)) {
+            return (T) value;
+        }
+        return JSON.parseObject(value, FastJson2Util.buildType(clazz));
+    }
+    
+    @Override
+    public void put(String key, Object value) {
+        String actual = value instanceof String ? (String) value : JSON.toJSONString(value);
+        stringRedisTemplate.opsForValue().set(key, actual, redisProperties.getValueTimout(), redisProperties.getValueTimeUnit());
+    }
+}
---
+++ mall4j-framework/mall4j-cache-spring-boot-starter/src/main/java/cn/mall4j/springboot/starter/cache/config/RedisDistributedProperties.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.mall4j.springboot.starter.cache.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 分布式缓存配置
+ */
+@Data
+@ConfigurationProperties(prefix = RedisDistributedProperties.PREFIX)
+public class RedisDistributedProperties {
+    
+    public static final String PREFIX = "spring.redis";
+    
+    /**
+     * 默认超时时间
+     */
+    private Long valueTimout = 30000L;
+    
+    /**
+     * 时间单位
+     */
+    private TimeUnit valueTimeUnit = TimeUnit.MILLISECONDS;
+}
