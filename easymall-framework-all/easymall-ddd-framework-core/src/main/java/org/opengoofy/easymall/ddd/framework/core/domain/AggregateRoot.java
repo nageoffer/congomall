de156cf8ceda528038344132ef214689b16af949
chen.ma <machencoding@163.com>
2022-09-18 20:03:59 +0800

    feature: DDD 基础模块增加实体和聚合根

---
+++ easymall-framework-all/easymall-ddd-framework-core/src/main/java/org/opengoofy/easymall/ddd/framework/core/domain/AggregateRoot.java
@@ -0,0 +1,10 @@
+package org.opengoofy.easymall.ddd.framework.core.domain;
+
+/**
+ * 聚合根
+ *
+ * @author chen.ma
+ * @github https://github.com/itmachen
+ */
+public interface AggregateRoot {
+}
---
+++ easymall-framework-all/easymall-ddd-framework-core/src/main/java/org/opengoofy/easymall/ddd/framework/core/domain/Entity.java
@@ -0,0 +1,10 @@
+package org.opengoofy.easymall.ddd.framework.core.domain;
+
+/**
+ * 实体
+ *
+ * @author chen.ma
+ * @github https://github.com/itmachen
+ */
+public interface Entity {
+}
