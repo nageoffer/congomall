fe40a603553d9d5bb3f169330145f1eb8429d5a9
chen.ma <machencoding@163.com>
2022-09-27 09:18:07 +0800

    feature: 雪花算法组成类添加基因字段

--- easymall-framework-all/easymall-distributedid-spring-boot-starter/src/main/java/org/opengoofy/easymall/springboot/starter/distributedid/core/SnowflakeIdInfo.java
+++ easymall-framework-all/easymall-distributedid-spring-boot-starter/src/main/java/org/opengoofy/easymall/springboot/starter/distributedid/core/SnowflakeIdInfo.java
@@ -53,4 +53,9 @@ public class SnowflakeIdInfo {
      * 自增序号，当高频模式下时，同一毫秒内生成 N 个 ID，则这个序号在同一毫秒下，自增以避免 ID 重复
      */
     private Integer sequence;
+    
+    /**
+     * 通过基因法生成的序号，会和 {@link SnowflakeIdInfo#sequence} 共占 12 bit
+     */
+    private Integer gene;
 }
