de900cc5e8947b67412b2ce38d34e44da7de1ba6
chen.ma <machencoding@163.com>
2023-01-03 20:40:43 +0800

    feature: 测试微服务流量监控，是否可以在配置全局异常拦截器的情况下，拦截业务异常

---
+++ congomall-test-all/congomall-flow-monitor-agent-test/congomall-flow-monitor-message-provider-test/src/main/java/org/opengoofy/congomall/test/flowmonitor/agent/message/provide/config/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.congomall.test.flowmonitor.agent.message.provide.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.opengoofy.congomall.springboot.starter.convention.exception.ErrorCode;
+import org.opengoofy.congomall.springboot.starter.convention.result.Result;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局异常拦截器
+ *
+ * @author chen.ma
+ * @github https://github.com/opengoofy
+ */
+@Slf4j
+@Component
+@RestControllerAdvice
+public final class GlobalExceptionHandler {
+    
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("{} {}", request.getMethod(), getUrl(request), throwable);
+        return new Result().setCode(ErrorCode.SERVICE_ERROR.code()).setMessage(ErrorCode.SERVICE_ERROR.message());
+    }
+    
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
