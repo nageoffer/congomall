d17416354a7e91d91d7842fb16c4ec21cbd7d5be
chen.ma <machencoding@163.com>
2023-01-07 11:25:55 +0800

    docs: 修改 @MLog 注解注释说明

--- congomall-framework-all/congomall-log-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/log/annotation/MLog.java
+++ congomall-framework-all/congomall-log-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/log/annotation/MLog.java
@@ -23,7 +23,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Log 注解打印
+ * Log 注解打印，可以标记在类或者方法上
+ * 标记在类上，类下所有方法都会打印；标记在方法上，仅打印标记方法；如果类或者方法上都有标记，以方法上注解为准
  *
  * @author chen.ma
  * @github https://github.com/opengoofy
@@ -35,14 +36,14 @@ public @interface MLog {
     /**
      * 入参打印
      *
-     * @return
+     * @return 打印结果中是否包含入参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
      */
     boolean input() default true;
     
     /**
      * 出参打印
      *
-     * @return
+     * @return 打印结果中是否包含出参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
      */
     boolean output() default true;
 }
