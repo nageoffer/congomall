6d31df8823c6010de2f6ce344c964f76053e1905
chen.ma <machencoding@163.com>
2023-01-10 19:01:06 +0800

    feature: 优化 SpringBoot 项目初次调用接口缓慢问题

--- congomall-framework-all/congomall-web-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/web/config/WebAutoConfiguration.java
+++ congomall-framework-all/congomall-web-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/web/config/WebAutoConfiguration.java
@@ -18,9 +18,15 @@
 package org.opengoofy.congomall.springboot.starter.web.config;
 
 import org.opengoofy.congomall.springboot.starter.web.GlobalExceptionHandler;
+import org.opengoofy.congomall.springboot.starter.web.initialize.InitializeDispatcherServletController;
+import org.opengoofy.congomall.springboot.starter.web.initialize.InitializeDispatcherServletHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Web 组件自动装配
@@ -31,9 +37,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebAutoConfiguration {
     
+    public final static String INITIALIZE_PATH = "/initialize/dispatcher-servlet";
+    
     @Bean
     @ConditionalOnMissingBean
     public GlobalExceptionHandler congoMallGlobalExceptionHandler() {
         return new GlobalExceptionHandler();
+    }
+    
+    @Bean
+    public InitializeDispatcherServletController initializeDispatcherServletController() {
+        return new InitializeDispatcherServletController();
+    }
+    
+    @Bean
+    public RestTemplate simpleRestTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+    
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+        return factory;
+    }
+    
+    @Bean
+    public InitializeDispatcherServletHandler initializeDispatcherServletHandler(RestTemplate simpleRestTemplate, ConfigurableEnvironment configurableEnvironment) {
+        return new InitializeDispatcherServletHandler(simpleRestTemplate, configurableEnvironment);
     }
 }
