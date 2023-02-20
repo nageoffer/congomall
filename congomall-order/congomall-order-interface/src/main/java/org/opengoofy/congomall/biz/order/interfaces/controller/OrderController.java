31d45b3383d23ee84a9ac2bf3174ae4c9a5b2418
chen.ma <machencoding@163.com>
2023-02-20 23:38:24 +0800

    feature: 订单服务下单和取消下单接口切换幂等注解

--- congomall-order/congomall-order-interface/src/main/java/org/opengoofy/congomall/biz/order/interfaces/controller/OrderController.java
+++ congomall-order/congomall-order-interface/src/main/java/org/opengoofy/congomall/biz/order/interfaces/controller/OrderController.java
@@ -24,11 +24,17 @@ import org.opengoofy.congomall.biz.order.application.req.OrderCreateCommand;
 import org.opengoofy.congomall.biz.order.application.resp.OrderRespDTO;
 import org.opengoofy.congomall.biz.order.application.service.OrderService;
 import org.opengoofy.congomall.springboot.starter.convention.result.Result;
-import org.opengoofy.congomall.springboot.starter.idempotent.annotation.RestAPIIdempotent;
+import org.opengoofy.congomall.springboot.starter.idempotent.annotation.Idempotent;
 import org.opengoofy.congomall.springboot.starter.idempotent.enums.IdempotentTypeEnum;
 import org.opengoofy.congomall.springboot.starter.log.annotation.MLog;
 import org.opengoofy.congomall.springboot.starter.web.Results;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -63,7 +69,7 @@ public class OrderController {
     
     @PostMapping
     @ApiOperation("商品订单下单")
-    @RestAPIIdempotent(type = IdempotentTypeEnum.PARAM, message = "订单已创建，请稍后再试")
+    @Idempotent(type = IdempotentTypeEnum.PARAM, message = "订单已创建，请稍后再试")
     public Result<String> createOrder(@RequestBody OrderCreateCommand requestParam) {
         String orderNo = orderService.createOrder(requestParam);
         return Results.success(orderNo);
@@ -71,7 +77,7 @@ public class OrderController {
     
     @PutMapping("/{orderSn}")
     @ApiOperation("商品订单取消")
-    @RestAPIIdempotent(type = IdempotentTypeEnum.TOKEN, message = "订单取消失败，请刷新订单状态或重新操作")
+    @Idempotent(type = IdempotentTypeEnum.TOKEN, message = "订单取消失败，请刷新订单状态或重新操作")
     public Result<Void> canalOrder(@PathVariable("orderSn") String orderSn) {
         orderService.canalOrder(orderSn);
         return Results.success();
