62ebfff8c2769279c0c024279a475a6e68c9311f
chen.ma <machen@apache.org>
2023-04-13 22:07:15 +0800

    feature: 商城网站 BFF 层开发首页商品展示 (#14)

---
+++ congomall-bff/congomall-bff-web/src/main/java/org/opengoofy/congomall/bff/web/controller/GoodsController.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.congomall.bff.web.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.opengoofy.congomall.bff.biz.common.ResultT;
+import org.opengoofy.congomall.bff.biz.dto.resp.adapter.HomePanelAdapterRespDTO;
+import org.opengoofy.congomall.bff.biz.service.GoodsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 商城首页控制层
+ *
+ * @author chen.ma
+ * @github <a href="https://github.com/opengoofy" />
+ * @公众号 马丁玩编程，关注回复：资料，领取后端技术专家成长手册
+ */
+@RestController
+@RequiredArgsConstructor
+public class GoodsController {
+    
+    private final GoodsService goodsService;
+    
+    @GetMapping("/goods/home")
+    @ApiOperation(value = "商城首页板块商品数据", notes = "商城首页板块商品数据")
+    public ResultT<List<HomePanelAdapterRespDTO>> goodsHome() {
+        return ResultT.success(goodsService.listHomePanel());
+    }
+}
