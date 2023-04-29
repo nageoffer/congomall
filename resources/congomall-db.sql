0f138b5207e6783b38e0e003734276cc4969ff20
chen.ma <machen@apache.org>
2023-04-29 11:09:15 +0800

    docs: 修改刚果商城初始化数据库 SQL 语句

--- resources/congomall-db.sql
+++ resources/congomall-db.sql
@@ -1,24 +1,31 @@
+-- 基础数据数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_basic_data;
 
-# 基础数据数据库
-CREATE DATABASE IF NOT EXISTS congomall_basic_data;
+-- 购物车数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_cart;
 
-# 购物车数据库
-CREATE DATABASE IF NOT EXISTS congomall_cart;
+-- 用户数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_customer_user;
 
-# 用户数据库
-CREATE DATABASE IF NOT EXISTS congomall_customer_user;
+-- 消息数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_message;
 
-# 消息数据库
-CREATE DATABASE IF NOT EXISTS congomall_message;
+-- 订单数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_order;
 
-# 订单数据库
-CREATE DATABASE IF NOT EXISTS congomall_order;
+-- 支付数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_pay;
 
-# 支付数据库
-CREATE DATABASE IF NOT EXISTS congomall_pay;
+-- 商品数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_product;
 
-# 商品数据库
-CREATE DATABASE IF NOT EXISTS congomall_product;
-
-# BFF聚合层数据库
-CREATE DATABASE IF NOT EXISTS congomall_bff;
+-- BFF聚合层数据库
+CREATE
+DATABASE IF NOT EXISTS congomall_bff;
