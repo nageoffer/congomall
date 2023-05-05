22013f7e4ee0f2240db608ac4475391602cdab15
chen.ma <machen@apache.org>
2023-05-05 21:45:00 +0800

    refature: 抽象责任链模式重构，初始化时排序提高运行时性能

--- congomall-framework-all/congomall-designpattern-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/designpattern/chain/AbstractChainContext.java
+++ congomall-framework-all/congomall-designpattern-spring-boot-starter/src/main/java/org/opengoofy/congomall/springboot/starter/designpattern/chain/AbstractChainContext.java
@@ -21,11 +21,13 @@ import com.google.common.collect.Maps;
 import org.opengoofy.congomall.springboot.starter.base.ApplicationContextHolder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.Ordered;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 抽象责任链上下文
@@ -41,23 +43,31 @@ public final class AbstractChainContext<T> implements CommandLineRunner {
     /**
      * 责任链组件执行
      *
+     * @param mark         责任链组件标识
      * @param requestParam 请求参数
      */
     public void handler(String mark, T requestParam) {
-        abstractChainHandlerContainer.get(mark).stream()
-                .sorted(Comparator.comparing(Ordered::getOrder)).forEach(each -> each.handler(requestParam));
+        List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(mark);
+        if (CollectionUtils.isEmpty(abstractChainHandlers)) {
+            throw new RuntimeException(String.format("[%s] Chain of Responsibility ID is undefined.", mark));
+        }
+        abstractChainHandlers.forEach(each -> each.handler(requestParam));
     }
     
     @Override
     public void run(String... args) throws Exception {
-        Map<String, AbstractChainHandler> chainFilterMap = ApplicationContextHolder.getBeansOfType(AbstractChainHandler.class);
+        Map<String, AbstractChainHandler> chainFilterMap = ApplicationContextHolder
+                .getBeansOfType(AbstractChainHandler.class);
         chainFilterMap.forEach((beanName, bean) -> {
             List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(bean.mark());
-            if (abstractChainHandlers == null) {
+            if (CollectionUtils.isEmpty(abstractChainHandlers)) {
                 abstractChainHandlers = new ArrayList();
             }
             abstractChainHandlers.add(bean);
-            abstractChainHandlerContainer.put(bean.mark(), abstractChainHandlers);
+            List<AbstractChainHandler> actualAbstractChainHandlers = abstractChainHandlers.stream()
+                    .sorted(Comparator.comparing(Ordered::getOrder))
+                    .collect(Collectors.toList());
+            abstractChainHandlerContainer.put(bean.mark(), actualAbstractChainHandlers);
         });
     }
 }
